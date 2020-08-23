e62a842cc3efa10d6a048a4ccc76abc178c202cc
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-08-23 01:59:21 -0500

    First pass at serve plugin

--- plugin/mod.ts
+++ plugin/mod.ts
@@ -2,10 +2,17 @@ export * from "./hooks.ts";
 export * from "./importResolver/mod.ts";
 export * from "./rootResolver/mod.ts";
 export * from "./chainResolver/mod.ts";
-export { pluginCacheLoader, Opts as CacheLoaderOptions } from "./cacheLoader/mod.ts";
-export { pluginFileLoader, Opts as FileLoaderOptions } from "./fileLoader/mod.ts";
+export {
+  pluginCacheLoader,
+  Opts as CacheLoaderOptions,
+} from "./cacheLoader/mod.ts";
+export {
+  pluginFileLoader,
+  Opts as FileLoaderOptions,
+} from "./fileLoader/mod.ts";
 export * from "./terserTransform/mod.ts";
 export * from "./typescriptTransform/mod.ts";
 export * from "./typescriptCompile/mod.ts";
 export * from "./cssBundle/mod.ts";
 export * from "./htmlBundle/mod.ts";
+export * from "./serve/mod.ts";
---
+++ plugin/serve/deps.ts
@@ -0,0 +1,5 @@
+export {
+  listenAndServe as createServer,
+  ServerRequest,
+} from "http://deno.land/std@0.66.0/http/mod.ts";
+export { contentType as getContentType } from "https://deno.land/x/media_types@v2.4.5/mod.ts";
