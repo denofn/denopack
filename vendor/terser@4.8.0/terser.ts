6199e8089bacce40f7f5f340f05103e3a4fcf345
Luca Casonato <lucacasonato@yahoo.com>
2020-09-08 12:14:08 +0200

    remove unneeded type import

--- vendor/terser@4.8.0/terser.ts
+++ vendor/terser@4.8.0/terser.ts
@@ -1,6 +1,6 @@
 import "https://unpkg.com/source-map@0.7.3/dist/source-map.js";
 import "https://unpkg.com/terser@5.3.0/dist/bundle.min.js";
 
-import { MinifyOptions, MinifyOutput, minify as minifyfn } from "./terser.d.ts";
+import type { minify as minifyfn } from "./terser.d.ts";
 
 export const minify: typeof minifyfn = (globalThis as any).Terser.minify;
