fba6b9429c667c5e5322d213aca92d2daba6baac
Luca Casonato <lucacasonato@yahoo.com>
2020-09-08 12:03:19 +0200

    correct typings

--- vendor/terser@4.8.0/terser.ts
+++ vendor/terser@4.8.0/terser.ts
@@ -1,9 +1,6 @@
 import "https://unpkg.com/source-map@0.7.3/dist/source-map.js";
 import "https://unpkg.com/terser@5.3.0/dist/bundle.min.js";
 
-import { MinifyOptions, MinifyOutput } from "./terser.d.ts";
+import { MinifyOptions, MinifyOutput, minify as minifyfn } from "./terser.d.ts";
 
-export const minify: (
-  files: string | string[] | { [file: string]: string },
-  options?: MinifyOptions
-) => MinifyOutput = (globalThis as any).Terser.minify;
+export const minify: typeof minifyfn = (globalThis as any).Terser.minify;
