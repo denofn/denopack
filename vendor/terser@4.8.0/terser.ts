002169a9bf0cab8713ccae322018f9f18fa7326d
Luca Casonato <lucacasonato@yahoo.com>
2020-09-08 11:58:28 +0200

    Update vendored terser

--- vendor/terser@4.8.0/terser.ts
+++ vendor/terser@4.8.0/terser.ts
@@ -1,9 +1,9 @@
 import "https://unpkg.com/source-map@0.7.3/dist/source-map.js";
-import "https://unpkg.com/terser@4.8.0/dist/bundle.min.js";
+import "https://unpkg.com/terser@5.3.0/dist/bundle.min.js";
 
-import { AST_Node, MinifyOptions, MinifyOutput } from "./terser.d.ts";
+import { MinifyOptions, MinifyOutput } from "./terser.d.ts";
 
 export const minify: (
-  files: string | string[] | { [file: string]: string } | AST_Node,
+  files: string | string[] | { [file: string]: string },
   options?: MinifyOptions
 ) => MinifyOutput = (globalThis as any).Terser.minify;
