ee330767ffa553d04d7f48d8e8bb74a11b404cda
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-09-08 23:47:08 -0500

    Fix relative path referencing in html files

--- util/htmlTemplate.ts
+++ util/htmlTemplate.ts
@@ -1,3 +1,4 @@
+import { path } from "../deps.ts";
 import { makeHtmlAttributes } from "./makeHtmlAttributes.ts";
 
 import type { OutputBundle } from "../deps.ts";
@@ -10,33 +11,49 @@ export type Attributes = {
 
 export type TemplateOpts = {
   attributes: Attributes;
+  bodyEntry?: string;
   bundle?: OutputBundle;
   files: Record<string, { fileName: string }[]>;
   meta: Record<string, string>[];
-  publicPath: string;
+  path: string;
   title: string;
-  bodyEntry?: string;
+};
+
+const resolveRelativePath = (from: string, to: string) => {
+  const raw_path = path.join(
+    path.relative(
+      path.dirname(from),
+      path.dirname(to),
+    ),
+    path.basename(to),
+  );
+
+  return raw_path.startsWith(".") ? raw_path : `./${raw_path}`;
 };
 
 export const htmlTemplate = async ({
   attributes,
   files,
   meta,
-  publicPath,
+  path,
   title,
   bodyEntry,
 }: TemplateOpts): Promise<string> => {
   const scripts = (files.js || [])
     .map(({ fileName }) => {
       const attrs = makeHtmlAttributes(attributes.script);
-      return `<script src="${publicPath}${fileName}"${attrs}></script>`;
+      return `<script src="${
+        resolveRelativePath(path, fileName)
+      }"${attrs}></script>`;
     })
     .join("\n    ");
 
   const links = (files.css || [])
     .map(({ fileName }) => {
       const attrs = makeHtmlAttributes(attributes.link);
-      return `<link href="${publicPath}${fileName}" rel="stylesheet"${attrs}>`;
+      return `<link href="${
+        resolveRelativePath(path, fileName)
+      }" rel="stylesheet"${attrs}>`;
     })
     .join("\n    ");
 
@@ -58,14 +75,15 @@ export const htmlTemplate = async ({
     ${scripts}
   </body>`;
 
-  return `
-<!doctype html>
-<html${makeHtmlAttributes(attributes.html)}>
-  <head>
-    ${metas}
-    <title>${title}</title>
-    ${links}
-  </head>
-${body}
-</html>`;
+  return (
+    `<!doctype html>
+      <html${makeHtmlAttributes(attributes.html)}>
+        <head>
+          ${metas}
+          <title>${title}</title>
+          ${links}
+        </head>
+      ${body}
+    </html>`
+  );
 };
