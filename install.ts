504d47fb7b4151c94cd1b593ac3454c549c595d8
Luca Casonato <lucacasonato@yahoo.com>
2020-09-16 09:33:24 +0200

    fix: installer doesn't exit early on failure (#26)

--- install.ts
+++ install.ts
@@ -1,4 +1,5 @@
 import { join, parse, resolve } from "https://deno.land/std@0.69.0/path/mod.ts";
+import { assert } from "https://deno.land/std@0.69.0/testing/asserts.ts";
 
 const url = import.meta.url;
 
@@ -17,56 +18,77 @@ function buildURL(target: string): string {
 // Cache all deps
 
 // Main deps
-await Deno.run({
-  cmd: ["deno", "cache", "--unstable", "--reload", `${buildURL("./deps.ts")}`],
-}).status();
+assert(
+  (await Deno.run({
+    cmd: [
+      "deno",
+      "cache",
+      "--unstable",
+      "--reload",
+      `${buildURL("./deps.ts")}`,
+    ],
+  }).status()).success,
+  "Failed to install dependencies.",
+);
 
 // Terser
-await Deno.run({
-  cmd: [
-    "deno",
-    "cache",
-    "--unstable",
-    "--reload",
-    `${buildURL("./plugin/terserTransform/deps.ts")}`,
-  ],
-}).status();
+assert(
+  (await Deno.run({
+    cmd: [
+      "deno",
+      "cache",
+      "--unstable",
+      "--reload",
+      `${buildURL("./plugin/terserTransform/deps.ts")}`,
+    ],
+  }).status()).success,
+  "Failed to install terserTransform dependencies.",
+);
 
 // Serve
-await Deno.run({
-  cmd: [
-    "deno",
-    "cache",
-    "--unstable",
-    "--reload",
-    `${buildURL("./plugin/serve/deps.ts")}`,
-  ],
-}).status();
+assert(
+  (await Deno.run({
+    cmd: [
+      "deno",
+      "cache",
+      "--unstable",
+      "--reload",
+      `${buildURL("./plugin/serve/deps.ts")}`,
+    ],
+  }).status()).success,
+  "Failed to install serve plugin dependencies.",
+);
 
 // Unstable plugin deps
-await Deno.run({
-  cmd: [
-    "deno",
-    "cache",
-    "--unstable",
-    "--no-check", // remove when https://github.com/denoland/deno/issues/7145 is resolved
-    "--reload",
-    `${buildURL("./plugin/deps.ts")}`,
-  ],
-}).status();
+assert(
+  (await Deno.run({
+    cmd: [
+      "deno",
+      "cache",
+      "--unstable",
+      "--no-check", // remove when https://github.com/denoland/deno/issues/7145 is resolved
+      "--reload",
+      `${buildURL("./plugin/deps.ts")}`,
+    ],
+  }).status()).success,
+  "Failed to install plugin dependencies.",
+);
 
 // Install
-await Deno.run({
-  cmd: [
-    "deno",
-    "install",
-    "--unstable",
-    "--allow-read",
-    "--allow-write",
-    "--allow-env",
-    "--allow-net",
-    "-f",
-    "--name=denopack",
-    `${buildURL("cli.ts")}`,
-  ],
-}).status();
+assert(
+  (await Deno.run({
+    cmd: [
+      "deno",
+      "install",
+      "--unstable",
+      "--allow-read",
+      "--allow-write",
+      "--allow-env",
+      "--allow-net",
+      "-f",
+      "--name=denopack",
+      `${buildURL("cli.ts")}`,
+    ],
+  }).status()).success,
+  "Failed to install denopack.",
+);
