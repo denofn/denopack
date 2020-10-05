1562dd2e94dedc66d8b27afb556b68e389cd2039
Luca Casonato <lucacasonato@yahoo.com>
2020-10-05 17:59:06 +0200

    normalize to file://

--- util/resolver.ts
+++ util/resolver.ts
@@ -5,13 +5,21 @@ export function resolver(
   importee: string,
   importer: string | undefined,
 ): string {
-  if (!importer) return importee;
-  else if (!usesProtocol(importee)) {
+  if (!importer) {
+    // If this is a raw absolute path, make it a `file:///` path.
+    if (!usesProtocol(importee) && path.isAbsolute(importee)) {
+      return new URL(`file:///${importee}`).toString();
+    }
+    return importee;
+  } else if (!usesProtocol(importee)) {
     // If this path is protocol prefixed, we can use URL to resolve it
     if (usesProtocol(importer)) return new URL(importee, importer).toString();
     // else if the path is absolute, return as is
-    if (path.isAbsolute(importee)) return importee;
+    if (path.isAbsolute(importee)) return resolver(importee, undefined);
     // else the path is relative, and we can resolve it by joining.
-    return path.join(path.parse(importer).dir, importee);
+    return resolver(
+      `file:///${path.join(path.parse(importer).dir, importee)}`,
+      undefined,
+    );
   } else return importee;
 }
