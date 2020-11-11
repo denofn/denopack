655747b7087d765b7bd5ab59f42f9389f2ad6e34
Luca Casonato <lucacasonato@yahoo.com>
2020-11-11 11:42:31 +0100

    chore: remove transpileOnly mutex lock

--- plugin/typescriptTransform/mod.ts
+++ plugin/typescriptTransform/mod.ts
@@ -1,19 +1,14 @@
 import type { Plugin } from "../../deps.ts";
-import Mutex from "../../util/mutex.ts";
-
-const transpilerMutex = new Mutex();
 
 export function pluginTypescriptTransform(opts?: Deno.CompilerOptions): Plugin {
   return {
     name: "denopack-plugin-typescriptTransform",
     async transform(code, id) {
-      const unlock = await transpilerMutex.lock();
       const result = await Deno.transpileOnly({ [id]: code }, opts).catch(
         (e) => {
           throw new Error(`Failed to transpile ${id}: ${e}`);
         },
       );
-      unlock();
       return { code: result[id].source, map: result[id].map };
     },
   };
