6842ee034beeceb3df6bb1aaf62bd7e06e257057
ohmlnz <othman.lanizi@gmail.com>
2020-12-09 15:10:36 -0500

    Fix: update to terser@5.3.0 to resolve shallow_cmp for chain expressions.

--- vendor/terser@5.3.1/terser.ts
+++ vendor/terser@5.3.1/terser.ts
@@ -1,5 +1,5 @@
 import "https://unpkg.com/source-map@0.7.3/dist/source-map.js";
-import "https://unpkg.com/terser@5.3.0/dist/bundle.min.js";
+import "https://unpkg.com/terser@5.3.1/dist/bundle.min.js";
 
 import type { minify as minifyfn } from "./terser.d.ts";
 
